7e9bfbb377e17e0a67209e113aa843b9e72f3ffc
Bohua Li <bohua.li.1984@gmail.com>
2020-06-01 19:29:52 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-curl --location --request POST 'ci-engine:3000/promote/app/9ebef745-8c39-430c-9b38-8d799ee8970f/dest/prod?targetAppId=0cf7d8cf-34fd-482a-993d-8fecc59c96ba&doReload=false&skipData=true&doPublish=false' \
+curl --location --request POST 'ci-engine:3000/promote/app/83a06053-c488-4040-8d6d-e6aa2bac2f1e/dest/prod?targetAppId=0cf7d8cf-34fd-482a-993d-8fecc59c96ba&doReload=false&skipData=true&doPublish=false' \
 --header 'qlik-user-id: BL' \
 --header 'qlik-user-directory: S-CUBED'
