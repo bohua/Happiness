9dc6c036d0792387eb802231c2b18074f499d990
Bohua Li <bohua.li.1984@gmail.com>
2020-06-01 19:29:31 +0200

    Update promote.sh

--- promote.sh
+++ promote.sh
@@ -1,3 +1,3 @@
-curl --location --request POST 'ci-engine:3000/promote/src/sit/app/9ebef745-8c39-430c-9b38-8d799ee8970f/dest/prod?targetAppId=0cf7d8cf-34fd-482a-993d-8fecc59c96ba&doReload=false&skipData=false&doPublish=false' \
+curl --location --request POST 'ci-engine:3000/promote/src/sit/app/83a06053-c488-4040-8d6d-e6aa2bac2f1e/dest/prod?targetAppId=0cf7d8cf-34fd-482a-993d-8fecc59c96ba&doReload=false&skipData=false&doPublish=false' \
 --header 'qlik-user-id: BL' \
 --header 'qlik-user-directory: S-CUBED'
