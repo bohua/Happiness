4984c7b318806b1509d58de054da20b4b8c5fbba
Bohua Li <bohua.li.1984@gmail.com>
2020-06-01 10:00:50 +0200

    Create promote.sh

---
+++ promote.sh
@@ -0,0 +1,4 @@
+curl --location --request POST 'localhost:3000/qlik/qrs/app/f4fd655c-dd1a-4d24-acd7-ea8ca4526e15/upload/replace?targetappid=d72b5fc3-1ed3-4fe3-bb97-73f6ba595d1c&keepdata=true' \
+--header 'qlik-host: prod' \
+--header 'qlik-user-id: BL' \
+--header 'qlik-user-directory: S-CUBED'
